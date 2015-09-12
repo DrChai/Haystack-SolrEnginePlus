2879714a950cb962cbe9a047f3dbda888e1d8527
Carrycat <Carrycat@bitbucket.org>
2015-09-11 21:24:03 -0400

    first commit

---
+++ backends/solr_backend.py
@@ -0,0 +1,100 @@
+from haystack.backends import BaseEngine
+from haystack.backends.solr_backend import SolrSearchBackend, SolrSearchQuery
+from haystack.constants import VALID_FILTERS, FILTER_SEPARATOR, DEFAULT_ALIAS, ITERATOR_LOAD_PER_QUERY
+
+__author__ = 'Carrycat'
+
+
+class EventSolrSearchBackend(SolrSearchBackend):
+
+    def _process_results(self, raw_results, highlight=False, result_class=None, distance_point=None):
+        result = super(EventSolrSearchBackend, self)._process_results(raw_results,
+                                                                      highlight=highlight,
+                                                                      result_class=result_class,
+                                                                      distance_point=distance_point)
+        if hasattr(raw_results, 'nextCursorMark'):
+            nextCursorMark = raw_results.nextCursorMark
+            result['nextCursorMark'] = nextCursorMark
+        return result
+
+    def build_search_kwargs(self, query_string, sort_by=None, start_offset=0, end_offset=None,
+                        fields='', highlight=False, facets=None,
+                        date_facets=None, query_facets=None,
+                        narrow_queries=None, spelling_query=None,
+                        within=None, dwithin=None, distance_point=None,
+                        models=None, limit_to_registered_models=None,
+                        result_class=None, stats=None, cursorMark=None, cursor_rows=None):
+        kwargs = super(EventSolrSearchBackend, self).build_search_kwargs(
+            query_string, sort_by=sort_by, start_offset=start_offset, end_offset=end_offset,
+            fields=fields, highlight=highlight, facets=facets,
+            date_facets=date_facets, query_facets=query_facets,
+            narrow_queries=narrow_queries, spelling_query=spelling_query,
+            within=within, dwithin=dwithin, distance_point=distance_point,
+            models=models, limit_to_registered_models=limit_to_registered_models,
+            result_class=result_class, stats=stats)
+        if cursorMark is not None:
+            kwargs['cursorMark'] = cursorMark
+            kwargs.pop('rows', None)
+            kwargs['rows'] = cursor_rows
+            kwargs.pop('start')
+        if kwargs.get('sort', None):
+            if kwargs['sort'] == 'geodist() asc':
+                kwargs['sort'] = 'geodist() asc,id asc'
+            elif kwargs['sort'] == 'geodist() desc':
+                kwargs['sort'] = 'geodist() desc,id desc'
+        return kwargs
+
+
+class EventSolrSearchQuery(SolrSearchQuery):
+
+    def __init__(self, using=DEFAULT_ALIAS):
+        self._next_cursor = None
+        self._current_cursor = None
+        self.cursor_rows = None
+        super(EventSolrSearchQuery, self).__init__(using=DEFAULT_ALIAS)
+
+    def build_params(self, spelling_query=None, **kwargs):
+        search_kwargs = super(EventSolrSearchQuery, self).build_params(spelling_query=spelling_query, **kwargs)
+        if self._next_cursor:  # if next_cursor() is called
+            # if self._next_cursor_cache:  # if cursor cache is set using cursor cache instead of next_cursor
+            #     search_kwargs['cursorMark'] = self._next_cursor_cache
+            # else:
+            search_kwargs['cursorMark'] = self._next_cursor
+            search_kwargs['cursor_rows'] = self.cursor_rows
+        return search_kwargs
+
+    def run(self, spelling_query=None, **kwargs):
+        """Builds and executes the query. Returns a list of search results."""
+        final_query = self.build_query()
+        search_kwargs = self.build_params(spelling_query, **kwargs)
+
+        if kwargs:
+            search_kwargs.update(kwargs)
+
+        results = self.backend.search(final_query, **search_kwargs)
+        self._results = results.get('results', [])
+        self._hit_count = results.get('hits', 0)
+        self._facet_counts = self.post_process_facets(results)
+        self._stats = results.get('stats', {})
+        if self._next_cursor:
+            self._current_cursor = self._next_cursor
+        self._next_cursor = results.get('nextCursorMark', None)  # update next cursor
+        #
+        # self._next_cursor_cache = results.get('nextCursorMark', None)  # X
+
+        self._spelling_suggestion = results.get('spelling_suggestion', None)
+
+    def add_next_cursor(self, next_cursor, rows):
+        """
+        set next cursorMark and rows
+        """
+        if not isinstance(next_cursor, str) or not isinstance(rows, int):
+            raise AttributeError('The next_cursor must be a string')
+        self.cursor_rows = rows
+        self._next_cursor = next_cursor
+
+
+
+class SolrEngine(BaseEngine):
+    backend = EventSolrSearchBackend
+    query = EventSolrSearchQuery
---
+++ query.py
@@ -0,0 +1,149 @@
+from haystack.constants import ITERATOR_LOAD_PER_QUERY
+from haystack.query import SearchQuerySet
+from django.utils import six
+
+__author__ = 'Carrycat'
+
+
+class CursorSearchQuerySet(SearchQuerySet):
+    def __init__(self, using=None, query=None):
+        self.is_using_cursor = False
+        self.is_cursor_cached = True
+        self.cursor_updated = False
+        super(CursorSearchQuerySet, self).__init__(using=using, query=query)
+
+    def __getitem__(self, k):
+        """
+        Retrieves an item or slice from the set of results.
+        """
+        if not isinstance(k, (slice, six.integer_types)):
+            raise TypeError
+        assert ((not isinstance(k, slice) and (k >= 0))
+                or (isinstance(k, slice) and (k.start is None or k.start >= 0)
+                    and (k.stop is None or k.stop >= 0))), \
+            "Negative indexing is not supported."
+
+        # Remember if it's a slice or not. We're going to treat everything as
+        # a slice to simply the logic and will `.pop()` at the end as needed.
+        if isinstance(k, slice):
+            is_slice = True
+            start = k.start
+
+            if k.stop is not None:
+                bound = int(k.stop)
+            else:
+                bound = None
+        else:
+            is_slice = False
+            start = k
+            bound = k + 1
+
+        # We need check to see if we need to populate more of the cache.
+        if len(self._result_cache) <= 0 or (None in self._result_cache[start:bound] and not self._cache_is_full()):
+            try:
+                if not self.query._next_cursor:
+                    self._fill_cache(start, bound)
+                else:
+                    current_cache_max = self._result_cache.index(None)
+                    bound = current_cache_max
+                    if start > bound:
+                        raise ValueError
+            except StopIteration:
+                # There's nothing left, even though the bound is higher.
+                pass
+
+        # Cache should be full enough for our needs.
+        if is_slice:
+            return self._result_cache[start:bound]
+        else:
+            return self._result_cache[start]
+
+    def set_next_cursor(self, next_cursor, rows=10, cached=False):
+        if cached:
+            this = self
+        else:
+            this = self._clone()
+        this.cursor_updated = True
+        if next_cursor != this.query._next_cursor:  # diff from last check point
+            this.is_cursor_cached = False
+        elif next_cursor == this.query._next_cursor:
+            this.is_cursor_cached = True
+        this.query.add_next_cursor(next_cursor, int(rows))
+        return this
+
+    def get_next_cursor(self):
+        if not self.query._next_cursor:
+            return None  # undefined or return ERROR
+        current_cache_max = 0
+        if len(self._result_cache) > 0:
+            try:
+                current_cache_max = self._result_cache.index(None)
+            except ValueError:
+                current_cache_max = len(self._result_cache)
+        if self.query._current_cursor is None or not self.is_cursor_cached:  # if its first time called or not at last check point
+            self._fill_cache(0, None)  # refill cache result
+        elif self.is_cursor_cached and self.cursor_updated:
+            self._fill_cache(current_cache_max, None)
+        return self.query._next_cursor
+
+    def _fill_cache(self, start, end, **kwargs):
+        # Tell the query where to start from and how many we'd like.
+        self.query._reset()
+        if self.query._next_cursor is None:
+            self.query.set_limits(start, end)
+
+        results = self.query.get_results(**kwargs)
+        self.cursor_updated = False
+        if results is None or len(results) == 0:
+            return False
+
+        # Setup the full cache now that we know how many results there are.
+        # We need the ``None``s as placeholders to know what parts of the
+        # cache we have/haven't filled.
+        # Using ``None`` like this takes up very little memory. In testing,
+        # an array of 100,000 ``None``s consumed less than .5 Mb, which ought
+        # to be an acceptable loss for consistent and more efficient caching.
+        if len(self._result_cache) == 0 or self.is_cursor_cached is False:
+            self._result_cache = [None for i in range(self.query.get_count())]
+
+        if start is None:
+            start = 0
+
+        if end is None:
+            end = self.query.get_count()
+
+        to_cache = self.post_process_results(results)
+
+        # Assign by slice.
+        self._result_cache[start:start + len(to_cache)] = to_cache
+        return True
+
+    def _manual_iter(self):
+        # If we're here, our cache isn't fully populated.
+        # For efficiency, fill the cache as we go if we run out of results.
+        # Also, this can't be part of the __iter__ method due to Python's rules
+        # about generator functions.
+        current_position = 0
+        current_cache_max = 0
+
+        while True:
+            if len(self._result_cache) > 0:
+                try:
+                    current_cache_max = self._result_cache.index(None)
+                except ValueError:
+                    current_cache_max = len(self._result_cache)
+
+            while current_position < current_cache_max:
+                yield self._result_cache[current_position]
+                current_position += 1
+
+            if self._cache_is_full():
+                raise StopIteration
+
+            if not self.cursor_updated:
+                # since we dont need _fill_cache() at the bottom
+                raise StopIteration
+            # We've run out of results and haven't hit our limit.
+            # Fill more of the cache.
+            if not self._fill_cache(current_position, current_position + ITERATOR_LOAD_PER_QUERY):
+                raise StopIteration
